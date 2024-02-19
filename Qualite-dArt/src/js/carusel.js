eab6068c775aa8ed21f4603a992a70d1458fcfcf
ReyMtzO <reyjazzmtz@gmail.com>
2024-02-18 21:06:25 -0600

    Proyecto de Ecommerce para casa productora QualiteDArt

---
+++ Qualite-dArt/src/js/carusel.js
@@ -0,0 +1,136 @@
+$(document).ready(function () {
+  $(".customers-logos").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $(".customer-logosv2").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
+});
+
+//
+//SERVICIOS
+////
+function moveToSelected(element) {
+  if (element == "next") {
+    var selected = $(".selected").next();
+  } else if (element == "prev") {
+    var selected = $(".selected").prev();
+  } else {
+    var selected = element;
+  }
+
+  var next = $(selected).next();
+  var prev = $(selected).prev();
+  var prevSecond = $(prev).prev();
+  var nextSecond = $(next).next();
+
+  $(selected).removeClass().addClass("selected");
+
+  $(prev).removeClass().addClass("prev");
+  $(next).removeClass().addClass("next");
+
+  $(nextSecond).removeClass().addClass("nextRightSecond");
+  $(prevSecond).removeClass().addClass("prevLeftSecond");
+
+  $(nextSecond).nextAll().removeClass().addClass("hideRight");
+  $(prevSecond).prevAll().removeClass().addClass("hideLeft");
+}
+
+// Eventos teclado
+$(document).keydown(function (e) {
+  switch (e.which) {
+    case 37: // left
+      moveToSelected("prev");
+      break;
+
+    case 39: // right
+      moveToSelected("next");
+      break;
+
+    default:
+      return;
+  }
+  e.preventDefault();
+});
+
+$("#carousel div").click(function () {
+  moveToSelected($(this));
+});
+
+$("#prev").click(function () {
+  moveToSelected("prev");
+});
+
+$("#next").click(function () {
+  moveToSelected("next");
+});
+
---
+++ README.md
@@ -0,0 +1,47 @@
+# **Proyect Qualite d'Art**
+## Bienvenidos al Repositorio de Qualite d'Art eCommerce 🛍️
+
+
+Este emocionante repositorio tiene un objetivo claro: el desarrollo y la creación de la magnífica plataforma de eCommerce **Qualite d'Art**. 
+¿Qué es Qualite d'Art? text
+
+## ¿Qué Puedes Encontrar Aquí?
+Desarrollo Activo: Este repositorio es el corazón de nuestra labor de desarrollo. Aquí encontrarás la evolución constante de la plataforma, con nuevas características y mejoras que buscan llevar la experiencia de solicitar un servicio de nuestros clientes a otro nivel.
+
+Equipo de Trabajo: Contamos con una rama principal main donde convergen nuestras contribuciones. Además, cada miembro de nuestro talentoso equipo tiene su propia rama personal para experimentar y desarrollar de manera individual antes de unir fuerzas en la rama principal.
+
+
+### Develop 
+| `REY` | `Xime` | `Rodrigo` | `Rocio` | `Cyndy` | `Diego` | `Uriel` | `Martin` |
+|:--------------|:-------------:|--------------:|--------------:|--------------:|--------------:|--------------:|--------------:|
+|<img src="/Devocados/AboutUs-devocados/assets/REY.png" width="80"/>| <img src="/Devocados/AboutUs-devocados/assets/Ximena Muñoz.jpg" width="80"/>| <img src="/Devocados/AboutUs-devocados/assets/rodo pro.jpg" width="80"/> | <img src="/Devocados/AboutUs-devocados/assets/Rocio Hdz.jpg" width="80"/> | <img src="/Devocados/AboutUs-devocados/assets/Cindy.jpg" width="80"/> |
+
+
+- `REY` https://github.com/ReyMtzO
+- `Xime` https://github.com/Ximonkey
+- `Rodrigo` https://github.com/RodrigoMacedoValencia
+- `Rocio` https://github.com/rhg10fl
+- `Cyndy` https://github.com/CindyRejon
+- `Diego` https://github.com/DiegoArturoG
+- `Uriel` https://github.com/urielcarortz
+- `Martin` https://github.com/monnsmonsh
+```sh
+
+  
+```
+
+## Technologies
+***
+Les presentamos las tecnologias usadas en este proyecto:
+* [JAVA](https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html): Version 17 
+* [JavaScript]
+* [Boostrap](https://getbootstrap.com/docs/5.3/getting-started/introduction/): Version 5.3.2 
+* [HTML5]
+* [CSS]
+
+Colaboración Abierta: ¡Este repositorio es público! Nos encanta compartir nuestro proceso y crecimiento con la comunidad. Siéntete libre de explorar, hacer sugerencias y, si lo deseas, contribuir con tus propias ideas y mejoras.
+Nuestro Compromiso
+En Qualite d'Art, no solo estamos construyendo una plataforma de servicion; 
+
+
+Únete a nosotros en este emocionante viaje para crear una experiencia de compra en línea que no solo sea emocionante, sino también impactante y consciente. ¡Es hora de llevar  tu evento a todos los rincones del mundo!
